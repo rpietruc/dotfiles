0e8b0ba5117a664419345f800c3bef3041a484f5
Rafal Pietruch <pietruchr@gmail.com>
2016-11-24 13:29:23 +0100

    install script added

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/spacemacs ~/.spacemacs
+
+mkdir -p ~/.config/openbox
+ln -s $(pwd)/openbox/autostart ~/.config/openbox/autostart
+ln -s $(pwd)/openbox/menu.xml ~/.config/openbox/menu.xml
+ln -s $(pwd)/openbox/rc.xml ~/.config/openbox/rc.xml
+
+echo "source $(pwd)/bashrc" >> ~/.bashrc
+
