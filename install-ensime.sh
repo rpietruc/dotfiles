f0702b41f772928612f71e430024d7d34783568a
Rafał Pietruch <pietruchr@gmail.com>
2019-01-05 16:54:39 +0100

    install java src for ensime

--- install-ensime.sh
+++ install-ensime.sh
@@ -2,4 +2,5 @@
 
 mkdir -p ~/.sbt/1.0/plugins/
 echo 'addSbtPlugin("org.ensime" % "sbt-ensime" % "2.5.1")' >> ~/.sbt/1.0/plugins/plugins.sbt
-
+yaourt -S openjdk-src
+echo "for each Scala project run: sbt configEnsime"
