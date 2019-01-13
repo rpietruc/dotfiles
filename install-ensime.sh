05d19cbefd574014af3713642c22e3d14657b794
Rafał Pietruch <pietruchr@gmail.com>
2019-01-13 15:18:06 +0100

    fix doc

--- install-ensime.sh
+++ install-ensime.sh
@@ -3,4 +3,4 @@
 mkdir -p ~/.sbt/1.0/plugins/
 echo 'addSbtPlugin("org.ensime" % "sbt-ensime" % "2.5.1")' >> ~/.sbt/1.0/plugins/plugins.sbt
 yaourt -S openjdk-src
-echo "for each Scala project run: sbt configEnsime"
+echo "for each Scala project run: sbt ensimeConfig"
