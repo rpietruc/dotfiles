3a22b41025e18512a29451a3101f61df11d21726
Rafal Pietruch <pietruchr@gmail.com>
2019-02-09 15:05:35 +0100

    akka, gke, evil mode

--- bashrc
+++ bashrc
@@ -492,4 +492,8 @@ function scala-minimal {
 }
 alias scala-hello-world='sbt new scala/hello-world.g8'
 alias scala-cats='sbt new underscoreio/cats-seed.g8'
+alias scala-akka='sbt new akka/akka-scala-seed.g8'
+alias scala-akka-http='sbt new akka/akka-http-quickstart-scala.g8'
+
+alias gke_start='gcloud container clusters get-credentials my-first-cluster-1 --zone europe-west2-c --project searchpy2'
 
--- emacs/init.el
+++ emacs/init.el
@@ -22,13 +22,14 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (w3 ensime))))
+ '(package-selected-packages (quote (evil w3 ensime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'dired-find-alternate-file 'disabled nil)
 
 (require 'evil)
   (evil-mode 1)
