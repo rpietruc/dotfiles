bdacadcfbe8f4445296610f154813060c0f694f5
Rafal Pietruch <pietruchr@gmail.com>
2020-11-14 20:50:53 +0100

    conky added

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ln -s $(pwd)/vimrc ~/.vimrc
 
-mkdir -p ~/.config/openbox
+mkdir -p ~/.config/openbox ~/.config/conky
 ln -s $(pwd)/openbox/autostart ~/.config/openbox/autostart
 ln -s $(pwd)/openbox/menu.xml ~/.config/openbox/menu.xml
 ln -s $(pwd)/openbox/rc.xml ~/.config/openbox/rc.xml
+ln -s $(pwd)/conky/conky.conf ~/.config/conky/conky.conf
 echo "source $(pwd)/bashrc" >> ~/.bashrc
 
