a60d4de6afdfe208a5754d3341cf212e5890535b
Rafal Pietruch <rpietruch@ichf.edu.pl>
2021-01-21 10:58:10 +0100

    jedi python autocomplete

--- emacs/init.el
+++ emacs/init.el
@@ -10,10 +10,11 @@
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("melpa-stable" . "http://stable.melpa.org/packages/")))
  '(package-selected-packages
-   '(py-isort python treemacs-projectile ac-haskell-process haskell-snippets sbt-mode lsp-metals magit scala-mode qml-mode treemacs haskell-mode)))
+   '(jedi py-isort python treemacs-projectile ac-haskell-process haskell-snippets sbt-mode lsp-metals magit scala-mode qml-mode treemacs haskell-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(add-hook 'python-mode-hook 'jedi:setup)
